887b9f65bb9748b07fc09ad4b31e6fa446faede7
Nikhil seth <nikhilseth30@gmail.com>
2023-03-26 02:08:38 +0530

    Basic-Project

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet, Text, Pressable } from 'react-native';
+import { useState } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App = () => {
+  const [bgColor, setBgColor] = useState(`rgb(202, 167, 212)`)
+    const onPressHandler = () =>{
+      const randomColor = `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`
+      console.log(randomColor)
+      setBgColor(randomColor)
+    }
+    const onPressHandler2 = () =>{
+      setBgColor("rgb(0, 0, 0)")
+    }
+
+
+    return <View style={[styles.container,{ backgroundColor:bgColor}]} >
+      <Pressable onPress={onPressHandler}><Text style={styles.text}>Press Me</Text></Pressable>
+      <Pressable onPress={onPressHandler2}><Text style={styles.text2}>Reset</Text></Pressable>
+      
     </View>
-  );
+  
 }
 
+export default App
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center",
+    backgroundColor:"rgb(202, 167, 212)"
   },
-});
+  text:{
+    padding:14,
+    paddingHorizontal:8,
+    paddingVertical:8, 
+    fontSize:20,
+    borderRadius:15,
+    backgroundColor:"rgb(202, 167, 212)",
+    color:"#fff"
+    },
+    text2:{
+      padding:14,
+      paddingHorizontal:8,
+      paddingVertical:8, 
+      fontSize:20,
+      borderRadius:15,
+      backgroundColor:"rgb(202, 167, 212)",
+      color:"#fff",
+      marginVertical:10
+      }
+
+})
